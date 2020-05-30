379da190e63d5c6f706337a3e4943a388b6cc1a3
Eastsky94 <dongminmin94@gmail.com>
2020-05-30 15:55:23 +0900

    edit mypage

--- app/src/main/java/com/hausung/hangil/MyPage/MyPageActivity.java
+++ app/src/main/java/com/hausung/hangil/MyPage/MyPageActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.hausung.hangil.R;
-import com.hausung.hangil.Reservation.MoreActivity;
+import com.hausung.hangil.StartActivity;
 import com.r0adkll.slidr.Slidr;
 import com.r0adkll.slidr.model.SlidrInterface;
 
 public class MyPageActivity extends AppCompatActivity {
+    FirebaseUser firebaseUser;
     private SlidrInterface slidr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +29,63 @@ public class MyPageActivity extends AppCompatActivity {
         //스와이프 코드
         slidr = Slidr.attach(this);
 
-        //로그아웃, 버전정보, 회원탈퇴 페이지로 이동
-        Button toMyPage2 = (Button) findViewById(R.id.toMyPageSecond);
-        toMyPage2.setOnClickListener(
+        //로그아웃, 버전정보, 회원탈퇴 페이지로 이동하는 버튼
+        Button toLogout=findViewById(R.id.toLogout);
+        Button toVersion=findViewById(R.id.toVersion);
+        Button toSecession=findViewById(R.id.toSecession);
+
+        toLogout.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        //MyPageSecondActivity로 가는 인텐트 생성
-                        Intent intent = new Intent(getApplication(), MyPageSecondActivity.class);
+                        //로그아웃 코드
+                        FirebaseAuth.getInstance().signOut();
+                        //StartActivity로 가는 인텐트 생성
+                        Intent intent = new Intent(getApplication(), StartActivity.class);
                         startActivity(intent);
                     }
                 }
         );
 
-        //각 예약현황 세부항목 보기
-        Button toMoreInfo = (Button) findViewById(R.id.moreInfo);
-        toMoreInfo.setOnClickListener(
+        toVersion.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        //MoreActivity로 가는 인텐트 생성
-                        Intent intent = new Intent(getApplication(), MoreActivity.class);
+                        Intent intent = new Intent(getApplication(), VersionViewActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        toSecession.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        // Get auth credentials from the user for re-authentication. The example below shows
+                        // email and password credentials but there are multiple possible providers,
+                        // such as GoogleAuthProvider or FacebookAuthProvider.
+                        AuthCredential credential = EmailAuthProvider
+                                .getCredential("user@example.com", "password1234");
+
+                        // Prompt the user to re-provide their sign-in credentials
+                        firebaseUser.reauthenticate(credential)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        // Log.d(TAG, "User re-authenticated.");
+                                    }
+                                });
+
+                        //회원탈퇴 코드 구현
+                        firebaseUser.delete()
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            //Log.d(TAG, "User account deleted.");
+                                        }
+                                    }
+                                });
+                        //firebaseUser=null;
+                        //StartActivity로 가는 인텐트 생성
+                        Intent intent = new Intent(getApplication(), StartActivity.class);
                         startActivity(intent);
                     }
                 }
